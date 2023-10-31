912f2952bda6e4673407c8c31e81158e9401a541
Riya <ris@narola.email>
2023-10-31 10:26:40 +0530

    table changes

--- src/app/custom-component/common-table/common-table.component.ts
+++ src/app/custom-component/common-table/common-table.component.ts
@@ -27,6 +27,7 @@ export class CommonTableComponent {
   pageIndex: any;
   pageLength: any;
   currentPageSize: any;
+  selectedChildRowIndex = null;
 
   @Input() columns: any;
   @Input() dataSource: any;
@@ -51,28 +52,11 @@ export class CommonTableComponent {
   @ViewChild('paginator', { static: false }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
 
+  selection = new SelectionModel<any>(true, []);
+
   //Test
   @ViewChild('childTableMenu') childTableMenu: MatMenuModule;
   @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
-
-  selection = new SelectionModel<any>(true, []);
-
-
-  childDataSource = [
-    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-    { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-    { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-    { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-    { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-    { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  ];
-
-
-  selectedChildRowIndex = null;
 
   constructor(
     private fb: FormBuilder,
@@ -89,8 +73,6 @@ export class CommonTableComponent {
     }
     this.displayedColumns.unshift('select');
     this.dataSource = new MatTableDataSource<any>(this.dataSource);
-
-
   }
 
   initialForm() {
@@ -108,7 +90,6 @@ export class CommonTableComponent {
   }
 
   AfterViewInit() {
-
     this.dataSource.paginator = this.paginator;
     this.sort.sort((this.defaultShortKey) as MatSortable);
     this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
